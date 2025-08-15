fdce3b9e39dfa781067214cbc9f595d33397cb6e
isaias-dev-7 <isaiasosoria7@gmail.com>
2025-08-14 21:08:18 -0400

    some fix on auth

---
+++ src/main/java/com/isaias/prospery_bussines_api/auth/decorators/GetUser.java
@@ -0,0 +1,10 @@
+package com.isaias.prospery_bussines_api.auth.decorators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GetUser {}
---
+++ src/main/java/com/isaias/prospery_bussines_api/auth/decorators/injectors/GetUserArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.isaias.prospery_bussines_api.auth.decorators.injectors;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.isaias.prospery_bussines_api.auth.decorators.GetUser;
+import com.isaias.prospery_bussines_api.user.entity.UserEntity;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class GetUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(GetUser.class)
+                && parameter.getParameterType().equals(UserEntity.class); // O UserEntity si usas ese tipo
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory)
+            throws Exception {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        System.out.println("PEPE"+request.getAttribute("User"));
+        UserEntity user = (UserEntity) request.getAttribute("User");
+
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found on request");
+        }
+
+        return user;
+    }
+
+}
