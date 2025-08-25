5f4b213abe7daf1625249f4bd26d58cdbb020b7b
isaias-dev-7 <isaiasosoria7@gmail.com>
2025-08-24 22:40:13 -0400

    swagger added

--- src/main/java/com/isaias/prospery_bussines_api/user/UserController.java
+++ src/main/java/com/isaias/prospery_bussines_api/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.isaias.prospery_bussines_api.common.dtos.PaginDto;
 import com.isaias.prospery_bussines_api.common.roles.RoleConstant;
 import com.isaias.prospery_bussines_api.user.dtos.UpdateUserPassDto;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @RestController
@@ -49,7 +51,7 @@ public class UserController {
             );
     }
 
-    @PostMapping("/{uuid}")
+    @PatchMapping("/{uuid}")
     @Auth({ RoleConstant.ADMIN, RoleConstant.SELLER, RoleConstant.USER })
     public ResponseEntity<?> updatePassword(
             @PathVariable String uuid,
