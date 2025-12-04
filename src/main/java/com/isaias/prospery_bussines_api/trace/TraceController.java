4c483123d1512f58e9d498d1ce5912e8ee433132
Isaias Elieser Osoria Tabares <isaiasosoria7@gmail.com>
2025-12-04 11:14:47 -0500

    created an entity for the traces

---
+++ src/main/java/com/isaias/prospery_bussines_api/trace/TraceController.java
@@ -0,0 +1,5 @@
+package com.isaias.prospery_bussines_api.trace;
+
+public class TraceController {
+    
+}
---
+++ src/main/java/com/isaias/prospery_bussines_api/trace/enums/TraceEnum.java
@@ -0,0 +1,7 @@
+package com.isaias.prospery_bussines_api.trace.enums;
+
+public enum TraceEnum {
+    CREATED,
+    DELETED,
+    UPDATED
+}
